cb9a71db599e8999089662d98d87a9e8bdbfe12e
Gabriel Capella <capellaresumo@gmail.com>
2014-05-23 22:19:32 -0300

    duvidas

--- js/controllers.js
+++ js/controllers.js
@@ -9,7 +9,7 @@ APP2.controller('DashCtrl', function($scope, $ionicModal, $location, $ionicLoadi
        console.log(comam.comando+'1');   
       }
     };
-    bluetoothSerial.isConnected(function(){}, function(){ $location.path( '/tab/account' ); });
+    bluetoothSerial.isConnected(function(){ console.log(23); }, function(){ $location.path( '/tab/account' ); });
 });
 
 APP2.controller('FriendsCtrl', function($scope, Friends) {
